f06fece9f8eaaecda1b4113441fe1f3ec6c17db7
AlinaKhusn <mingazova_al@mail.ru>
2023-06-06 19:18:50 +0300

    решение

--- task3.py
+++ task3.py
@@ -4,5 +4,20 @@
 # Нужно найти сумму трёх самых дорогих покупок, которые запишутся в переменную three_most_expensive_purchases
 
 # Здесь пишем код
+#f = open('test_file/task_3.txt', 'r', encoding="utf8")
+#while f.readline().isdigit():
+
+file = open("test_file/task_3.txt", "r", encoding="utf-8")
+total = 0
+buy = []
+for i in file.readlines():
+    if i != '\n':
+        total += int(i)
+    else:
+        buy.append(total)
+        total = 0
+buy.sort()
+three_most_expensive_purchases = sum(buy[-3::])
+file.close
 
 assert three_most_expensive_purchases == 202346
